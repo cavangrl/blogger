6ba5aea941fa776df91104b2c036b72932091ec1
cavangrl <pae.oconnor@gmail.com>
2014-07-02 16:50:14 -0400

    articles seem to be finished!!!

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,10 @@
 class ArticlesController < ApplicationController
 	 include ArticlesHelper
+   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
 
  def index
- 	@articles = Article.all
+ 	@article = Article.all
  end
 
  def show
@@ -17,9 +18,9 @@ end
 end
 
 
- def create
- 	@article = Article.new(params[:article])
-    
+  def create
+    @article = Article.new(article_params)
+
     @article.save
 
     redirect_to article_path(@article)
@@ -28,7 +29,39 @@ end
 
 
 
+  def destroy
+    @article = Article.find(params[:id])
 
+    @article.destroy
 
+    redirect_to articles_path
+  end
+
+  def edit
+   @article = Article.find(params[:id])
+
+  end
+
+  def update
+  	
+if @article.update(article_params)
+      flash[:notice] = "Article updated."
+      redirect_to @article
+    else
+      flash[:alert] = "There was a problem. Please try again."
+      redirect_to article_path(@article)
+    end
+  end
+
+   
+private
+
+  def article_params
+    params.require(:article).permit(:body)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
 end
